e57ad290dfd102364b44e0376a867ff276afd3e6
SukumarSun <msukumarsundar12@gmail.com>
2022-08-15 14:36:52 +0000

    namedexportjs

---
+++ importMultipleValues.mjs
@@ -0,0 +1,6 @@
+// import multiplyByFour from "./exportMultipleValues.mjs";
+import { multiplyByFour, myArray, bulb } from "./exportMultipleValues.mjs";
+// import bulb from "./exportMultipleValues.mjs";
+console.log(myArray);
+console.log(bulb);
+console.log(multiplyByFour(1));
